6f986891ee3bab25edd94d0f51ed7bb36538b970
TrongHieu <tronghieu170398@gmail.com>
2021-07-12 22:17:44 +0700

    create

---
+++ js/Script.js
@@ -0,0 +1,17 @@
+﻿$(function() {
+  $('.carousel').carousel({
+    interval: 3300
+  });
+
+   //Tự động active hình thumb trong main-carousel
+   $('#main-carousel-container').on('slide.bs.carousel', function (e) {
+    // Lấy vị trí slide mới
+    var index = e.to;
+
+    // Bỏ chọn active trên tất cả hình thumb
+    $("#main-carousel .thumb .coll").removeClass("active");
+    //Tìm hình thumb tại vị trí mới, active nó
+    $("#main-carousel .thumb .coll").eq(index).addClass("active");
+
+})
+});
